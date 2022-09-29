5c09aa95b79e73256555c2db62eff34b87a3806b
QuangNhat2803 <quangnhat280300@gmail.com>
2022-09-29 14:36:51 +0700

    update button in section rules & fix padding

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
-    var modal = document.getElementById("myModal");
-    var btn = document.getElementById("myBtn");
-    var span = document.getElementsByClassName("modal-close")[0];
+$(document).on('click', 'video, #btnplay, #btnreplay', function (e) {
+  var pl = document.getElementById('btnplay')
+  var rpl = document.getElementById('btnreplay')
+  var video = $('video').get(0);  // This line has been updated.
+  if (video.paused === false) {
+      video.pause();
+      pl.style='display: block';
+      }else if (video.paused === true)  {
+      video.play();
+      pl.style='display: none';
+      rpl.style='display: none';
+      } else {
+        rpl.style='display: block';
+      }
+      
+  return false;
+});
+    
+    
+    
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("modal-close")[0];
 
-    btn.onclick = function() {
-        modal.style.display = "block";
-      }
-      span.onclick = function() {
-        modal.style.display = "none";
-      }
-      window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-      }
+btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }   
+
+      
+
+
+    
